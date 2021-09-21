56b0288742b95d8ea0daa7d0485f83b2192e2f97
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-09-21 23:15:32 +0500

    changed font of whole project to rubik

--- src/views/Superadmin/Category.vue
+++ src/views/Superadmin/Category.vue
@@ -1,90 +1,95 @@
 <template>
-  <section>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-text-field
-            v-model="category.name_tm"
-            outlined
-            :label="$t('nameInTurkmen')"
-          />
-        </v-col>
-        <v-col cols="12">
-          <v-text-field
-            v-model="category.name_ru"
-            outlined
-            :label="$t('nameInRussian')"
-          />
-        </v-col>
-        <v-col cols="12">
-          <v-file-input
-            v-model="category.image"
-            outlined
-            :label="$t('updateImage')"
-          />
-        </v-col>
-        <v-btn
-          @click="update(category)"
-          block
-          color="primary"
-        >
-          {{ $t('updateCategory') }}
-        </v-btn>
-      </v-row>
-    </v-container>
-    <v-snackbar
-      app
-      absolute
-      bottom
-      right
-      timeout="4000"
-      color="primary"
-      dark
-      elevation="7"
-      v-model="hasAlert"
-      max-width="400"
-    >
-      {{ alert }}
-    </v-snackbar>
-  </section>
+	<section>
+		<v-container>
+			<v-row>
+				<v-col cols="12" md="6" class="d-flex align-center justify-center">
+					<div>
+						<v-img
+							:src="category.image"
+							:height="200"
+							:width="200"
+							contain
+							class="ma-5"
+						>
+							<template v-slot:placeholder>
+								<v-skeleton-loader type="image" />
+							</template>
+						</v-img>
+					</div>
+				</v-col>
+				<v-col cols="12" md="6" class="d-flex align-center justify-center">
+					<div style="width: 100%;">
+						<v-text-field
+							v-model="category.name_tm"
+							outlined
+							:label="$t('nameInTurkmen')"
+						/>
+						<v-text-field
+							v-model="category.name_ru"
+							outlined
+							:label="$t('nameInRussian')"
+						/>
+					</div>
+				</v-col>
+				<v-btn @click="update(category)" block color="primary">
+					{{ $t('updateCategory') }}
+				</v-btn>
+			</v-row>
+		</v-container>
+		<v-snackbar
+			app
+			absolute
+			bottom
+			right
+			timeout="4000"
+			color="primary"
+			dark
+			elevation="7"
+			v-model="hasAlert"
+			max-width="400"
+		>
+			{{ alert }}
+		</v-snackbar>
+	</section>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex';
 
 export default {
-  data() {
-    return {
-      alert: '',
-      loading: false,
-      hasAlert: false,
-    }
-  },
-  computed: {
-    ...mapState(['category']),
-  },
-  methods: {
-    ...mapActions(['fetchCategory']),
-    update(category) {
-      this.$axios.put(`/superadmin/categories/${category.id}`, category)
-        .then(() => {
-          this.dialog = false;
-          this.showAlert(this.$t('successfullyUpdated'));
-          this.$router.go(-1);
-        })
-        .catch(err => {
-          this.showAlert(err.message);
-        });
-    },
-    showAlert(mes) {
-      this.loading = false;
-      this.alert = mes;
-      this.hasAlert = true;
-    },
-  },
-  created() {
-    const id = this.$route.params.id
-    this.fetchCategory(id);
-  },
-}
+	data() {
+		return {
+			alert: '',
+			loading: false,
+			hasAlert: false,
+		};
+	},
+	computed: {
+		...mapState(['category']),
+	},
+	methods: {
+		...mapActions(['fetchCategory']),
+		update(category) {
+			this.$axios
+				.put(`/superadmin/categories/${category.id}`, category)
+				.then(() => {
+					this.dialog = false;
+					this.showAlert(this.$t('successfullyUpdated'));
+					this.$router.go(-1);
+				})
+				.catch(err => {
+					this.showAlert(err.message);
+				});
+		},
+		showAlert(mes) {
+			this.loading = false;
+			this.alert = mes;
+			this.hasAlert = true;
+		},
+	},
+	created() {
+		const id = this.$route.params.id;
+		this.fetchCategory(id);
+	},
+};
 </script>
