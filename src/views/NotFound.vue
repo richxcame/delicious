74a3ae45bb7e693d89d698e012994525ba9b9b81
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-09-23 00:04:33 +0500

    added link to notfound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,8 @@
 <template>
-  <v-app>
-    <div class="fill-height d-flex align-center justify-center">
-      <v-img
-        src="../assets/warning.svg"
-        max-height="400"
-        max-width="400"
-      />
-    </div>
-  </v-app>
+	<v-app>
+		<div class="fill-height d-flex align-center justify-center">
+			<v-img src="../assets/warning.svg" max-height="400" max-width="400" />
+			<router-link to="/login">{{ $t('goToLoginPage') }}</router-link>
+		</div>
+	</v-app>
 </template>
