bfbf0d0dc00bb5156c77588ffe20f0abfdce218e
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-09-23 16:39:26 +0500

    some changes in admin layout and home page

--- src/views/Admin/Home.vue
+++ src/views/Admin/Home.vue
@@ -78,120 +78,40 @@
 				></v-sparkline>
 			</v-container>
 		</v-row>
-
-		<v-row>
-			<v-container>
-				<v-card>
-					<v-card-title>
-						Orders
-						<v-spacer />
-						<v-text-field
-							v-model="search"
-							append-icon="mdi-magnify"
-							label="Search"
-							outlined
-							dense
-							single-line
-							hide-details
-						></v-text-field>
-					</v-card-title>
-					<v-data-table
-						:headers="orderHeaders"
-						:items="orderItems"
-						sort-by="deliveredAt"
-						:search="search"
-					>
-						<template v-slot:[`item.isDelivered`]="{ item }">
-							<v-switch v-model="item.isDelivered"></v-switch>
-							<!-- {{ item.isDelivered }} -->
-						</template>
-					</v-data-table>
-				</v-card>
-			</v-container>
-		</v-row>
 	</section>
 </template>
 
 <script>
 export default {
-	name: 'Superadmin',
-	data: () => ({
-		search: '',
-		orders: [],
-		info: [
-			{
-				icon: 'mdi-monitor-cellphone',
-				title: 'New visits',
-				color: 'cyan',
-				number: 10231,
-			},
-			{
-				icon: 'mdi-message-outline',
-				title: 'Messages',
-				color: 'blue darken-2',
-				number: 2301,
-			},
-			{
-				icon: 'mdi-account-outline',
-				title: 'Users',
-				color: 'red darken-1',
-				number: 500,
-			},
-			{
-				icon: 'mdi-cart-outline',
-				title: 'Shoppings',
-				color: 'cyan',
-				number: 9991,
-			},
-		],
-		orderHeaders: [
-			{
-				text: 'Shipping Address',
-				align: 'start',
-				sortable: false,
-				value: 'shippingAddress',
-			},
-			{ text: 'Delivered At', value: 'deliveredAt' },
-			{ text: 'Delivered', value: 'isDelivered' },
-			{
-				text: 'Total Price',
-				align: 'center',
-				value: 'totalPrice',
-				sortable: true,
-			},
-		],
-		orderItems: [
-			{
-				shippingAddress: 'Gorogly 113, AZC',
-				deliveredAt: '25.07.2021 21:01',
-				isDelivered: false,
-				totalPrice: 215,
-			},
-			{
-				shippingAddress: 'Ataturk 1972',
-				deliveredAt: '25.07.2021 23:01',
-				isDelivered: false,
-				totalPrice: 215,
-			},
-			{
-				shippingAddress: 'Aytakow',
-				deliveredAt: '25.07.2021 21:10',
-				isDelivered: true,
-				totalPrice: 215,
-			},
-		],
-	}),
-	created() {
-		this.$axios
-			.get('/superadmin/orders')
-			.then(res => {
-				console.log(res);
-				this.orders = res.data;
-				console.log(this.orders);
-			})
-			.catch(err => {
-				console.log(err);
-			});
+	data() {
+		return {
+			info: [
+				{
+					icon: 'mdi-monitor-cellphone',
+					title: 'New visits',
+					color: 'cyan',
+					number: 10231,
+				},
+				{
+					icon: 'mdi-message-outline',
+					title: 'Messages',
+					color: 'blue darken-2',
+					number: 2301,
+				},
+				{
+					icon: 'mdi-account-outline',
+					title: 'Users',
+					color: 'red darken-1',
+					number: 500,
+				},
+				{
+					icon: 'mdi-cart-outline',
+					title: 'Shoppings',
+					color: 'cyan',
+					number: 9991,
+				},
+			],
+		};
 	},
 };
 </script>
