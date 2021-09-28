aa13e00891101a0dda711c536ef8abbcd5205ee1
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-09-28 19:11:46 +0500

    creating users with superadmin

--- src/views/Superadmin/Users.vue
+++ src/views/Superadmin/Users.vue
@@ -1,51 +1,74 @@
 <template>
 	<section>
-		<v-row class="my-7">
-			<v-col cols="12" md="4">
-				<v-text-field
-					outlined
-					dense
-					v-model="user.name"
-					:label="$t('name')"
+		<v-form ref="form" v-model="isValid" lazy-validation>
+			<v-row class="my-7">
+				<v-col cols="12" md="4">
+					<v-text-field
+						outlined
+						dense
+						v-model="user.name"
+						:label="$t('name')"
+						color="primary"
+						required
+						:rules="requiredRule"
+						auto-complete="off"
+					/>
+				</v-col>
+				<v-col cols="12" md="4">
+					<v-text-field
+						outlined
+						dense
+						v-model="user.phoneNumber"
+						:label="$t('phoneNumber')"
+						color="primary"
+						required
+						:rules="phoneRules"
+						auto-complete="off"
+					>
+						<template #prepend-inner>
+							<div class="mt-1">
+								+993
+							</div>
+						</template>
+					</v-text-field>
+				</v-col>
+				<v-col cols="12" md="4">
+					<v-text-field
+						outlined
+						dense
+						v-model="user.password"
+						:label="$t('password')"
+						color="primary"
+						required
+						:rules="passwordRules"
+						:type="isPassword ? 'password' : 'text'"
+						auto-complete="off"
+					>
+						<template v-slot:append>
+							<v-icon @click="isPassword = !isPassword">
+								{{ isPassword ? 'mdi-eye-off-outline' : 'mdi-eye-outline' }}
+							</v-icon>
+						</template>
+					</v-text-field>
+				</v-col>
+				<v-btn
 					color="primary"
-				/>
-			</v-col>
-			<v-col cols="12" md="4">
-				<v-text-field
-					outlined
-					dense
-					v-model="user.phoneNumber"
-					:label="$t('phoneNumber')"
-					color="primary"
-				/>
-			</v-col>
-			<v-col cols="12" md="4">
-				<v-text-field
-					outlined
-					dense
-					v-model="user.password"
-					:label="$t('password')"
-					color="primary"
-				/>
-			</v-col>
-			<v-btn
-				color="primary"
-				block
-				height="45"
-				@click="setUser"
-				:loading="isLoading"
-				disabled
-			>
-				{{ $t('addUser') }}
-				<template v-slot:loader>
-					<span class="custom-loader">
-						<v-icon light>
-							mdi-cached
-						</v-icon>
-					</span>
-				</template>
-			</v-btn>
-		</v-row>
+					block
+					height="45"
+					@click="validate"
+					:loading="isLoading"
+				>
+					{{ $t('addUser') }}
+					<template v-slot:loader>
+						<span class="custom-loader">
+							<v-icon light>
+								mdi-cached
+							</v-icon>
+						</span>
+					</template>
+				</v-btn>
+			</v-row>
+		</v-form>
 		<v-card class="my-5">
 			<v-card-title>
 				{{ $tc('user', 2) }}
@@ -119,10 +142,21 @@ export default {
 					sortable: false,
 				},
 			],
+			isValid: true,
+			isPassword: true,
 			search: '',
 			isLoading: false,
 			hasAlert: false,
 			alert: '',
+			requiredRule: [v => !!v || this.$t('requiredField')],
+			phoneRules: [
+				v => !!v || this.$t('phoneRequired'),
+				v => (v.length === 8 && !/\D/.test(v)) || this.$t('phoneMustValid'),
+			],
+			passwordRules: [
+				v => !!v || this.$t('passwordRequired'),
+				v => v.length >= 6 || this.$t('lessPassword', { c: 6 }),
+			],
 		};
 	},
 	computed: {
@@ -156,9 +190,14 @@ export default {
 				return 'success';
 			}
 		},
+		validate() {
+			if (this.$refs.form.validate()) {
+				this.setUser();
+			}
+		},
 		setUser() {
 			this.$axios
-				.post('/superadmin/users', this.user)
+				.post('/auth/signup', this.user)
 				.then(() => {
 					this.showAlert(this.$t('successfullyCreated'));
 				})
